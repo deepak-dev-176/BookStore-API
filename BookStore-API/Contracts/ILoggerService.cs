854e6bac2f101d52d9c697ba71605f09502b0695
Deepak Janardhanan <deepakj@metaplore.com>
2021-05-11 01:04:22 +0530

    Add project files.

---
+++ BookStore-API/Contracts/ILoggerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Contracts
+{
+    public interface ILoggerService
+    {
+        public void LogInfo(string message); 
+        
+        public void LogWarn(string message); 
+        
+        public void LogDebug(string message); 
+        
+        public void LogError(string message); 
+    }
+}
